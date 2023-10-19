84f32075d13edd3920d0a23f991c3f0861c82fa9
Tiago Leite <contato@tiagocreator.com>
2023-10-19 08:46:45 -0300

    Add chakra provider and react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { Home, About, Services, Reviews, Contact } from './pages';
+import { Navbar } from './components';
+
 import './App.css';
 
 function App() {
-  return <>Hello, world!</>;
+  return (
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sobre' element={<About />} />
+          <Route path='/servicos' element={<Services />} />
+          <Route path='/avaliacoes' element={<Reviews />} />
+          <Route path='/contato' element={<Contact />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  );
 }
 
 export default App;

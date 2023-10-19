50425b4dd90608af8bbe7519411ebb3bdca4747e
Tiago Leite <contato@tiagocreator.com>
2023-10-19 08:45:01 -0300

    Create default export pages

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Services } from './Services';
+export { default as Reviews } from './Reviews';
+export { default as Contact } from './Contact';

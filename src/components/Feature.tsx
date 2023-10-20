61badefe38e4ce05ba11a418515c9e0eba78c724
Tiago Leite <contato@tiagocreator.com>
2023-10-20 13:57:30 -0300

    Create feature component

---
+++ src/components/Feature.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { chakra, Icon, Flex } from '@chakra-ui/react';
+
+import { IoCheckmark } from 'react-icons/io5';
+
+interface FeatureProps {
+  children: React.ReactNode;
+}
+
+const Feature: React.FC<FeatureProps> = (props) => {
+  return (
+    <Flex alignSelf='start' w='full'>
+      <Icon
+        boxSize={5}
+        mt={1}
+        mr={2}
+        _light={{
+          color: 'green.500',
+        }}
+        viewBox='0 0 20 20'
+        fill='currentColor'
+        as={IoCheckmark}
+      />
+      <chakra.p fontSize='lg' color='gray.600' {...props} />
+    </Flex>
+  );
+};
+export default Feature;

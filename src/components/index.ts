11c86af12327cadc7cf5f1fd7b22bc3c7610ee57
Tiago Leite <contato@tiagocreator.com>
2023-10-22 17:19:21 -0300

    Update default exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as MainHero } from './MainHero';
 export { default as Cta } from './Cta';
 export { default as Footer } from './Footer';
 export { default as Hero } from './Hero';
+export { default as CompanyInfo } from './CompanyInfo';

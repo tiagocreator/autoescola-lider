5bd8752d944e3e5ab3ac353160058941c6349654
Tiago Leite <contato@tiagocreator.com>
2023-10-22 14:26:21 -0300

    Create reviews page

---
+++ src/pages/Reviews.tsx
@@ -0,0 +1,12 @@
+import { Cta, Hero } from '../components';
+
+const Reviews = () => {
+  return (
+    <main>
+      <Hero title={'Testemunhas'} />
+      <Cta />
+    </main>
+  );
+};
+
+export default Reviews;

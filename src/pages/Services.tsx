e4e6c49c90371e5dbbf4c1dd653f421e11d517fd
Tiago Leite <contato@tiagocreator.com>
2023-10-22 14:26:30 -0300

    Create services page

---
+++ src/pages/Services.tsx
@@ -0,0 +1,12 @@
+import { Cta, Hero } from '../components';
+
+const Services = () => {
+  return (
+    <main>
+      <Hero title={'Serviços'} />
+      <Cta />
+    </main>
+  );
+};
+
+export default Services;

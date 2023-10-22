1e3334cf9d028b4ec9d7ad709563a9402ae7a6bf
Tiago Leite <contato@tiagocreator.com>
2023-10-22 14:26:13 -0300

    Create contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import { Cta, Hero } from '../components';
+
+const Contact = () => {
+  return (
+    <main>
+      <Hero title={'Contato'} />
+      <Cta />
+    </main>
+  );
+};
+
+export default Contact;

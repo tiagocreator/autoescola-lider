44efd7bf5da3128a7295aa793fe58060d3656652
Tiago Leite <contato@tiagocreator.com>
2023-10-22 14:26:00 -0300

    Create about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import { Cta, Hero } from '../components';
+
+const About = () => {
+  return (
+    <main>
+      <Hero title={'Quem Somos'} />
+      <Cta />
+    </main>
+  );
+};
+
+export default About;

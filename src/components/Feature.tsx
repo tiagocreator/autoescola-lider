0964cd9f0cb1f46e5b9bb80ddffca100cfeab4b2
Tiago Leite <contato@tiagocreator.com>
2023-10-31 08:53:54 -0300

    Update feature component styles

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -14,14 +14,12 @@ const Feature: React.FC<FeatureProps> = (props) => {
         boxSize={5}
         mt={1}
         mr={2}
-        _light={{
-          color: 'green.500',
-        }}
+        color='green.500'
         viewBox='0 0 20 20'
         fill='currentColor'
         as={IoCheckmark}
       />
-      <chakra.p fontSize='lg' color='gray.600' {...props} />
+      <chakra.p fontSize='lg' color='gray.800' {...props} />
     </Flex>
   );
 };

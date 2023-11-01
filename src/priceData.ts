aa21fde1e4fe5b28de5331d9dd418eb1d9d24e73
Tiago Leite <contato@tiagocreator.com>
2023-11-01 19:50:47 -0300

    Create price data object

---
+++ src/priceData.ts
@@ -0,0 +1,6 @@
+export const priceData: Record<string, { price: string; divided?: string }> = {
+  catA: { price: 'R$1548,00', divided: '10x R$165' },
+  catB: { price: 'R$1648,00', divided: '10x R$175' },
+  catAB: { price: 'R$2058,00', divided: '10x R$220' },
+  recycle: { price: 'R$500,00' },
+};
